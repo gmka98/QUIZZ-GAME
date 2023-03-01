e66976c6f410dc1de9cb5a9bb023e6ef735dea2a
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2023-03-01 04:38:03 +0100

    Update Quizz_game.py

--- Easy/Quizz_game.py
+++ Easy/Quizz_game.py
@@ -1,78 +1,68 @@
-print("Welcome to my Quizzz GAAAAme!")
+print("Welcome to my Quiz Game!")
 
-playing = input("Are you REEEEEAAAAAAAADYYYYYYYYY!? ")
+playing = input("Are you ready? ")
 
-if playing.upper() != "yes":
+if playing.lower() != "yes":
     quit()
 else:
-print("Let's GOOOOOOO!")    
+    print("Let's go!")    
+
 score = 0
 
-answer = input("Who work as a backend and as a front-end ?")
-if answer.upper == "A full stack developer":
+answer = input("Who works as both a backend and front-end developer? ")
+if answer.lower() == "a full stack developer":
     print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!") 
 
-answer = input()
-if answer.upper == "A full stack developer":
+answer = input("What language is commonly used for web development? ")
+if answer.lower() == "javascript":
     print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!")   
 
-
-answer = input()
-if answer.upper == "A full stack developer":
+answer = input("What does CSS stand for? ")
+if answer.lower() == "cascading style sheets":
     print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!") 
 
-answer = input()
-if answer.upper == "A full stack developer":
+answer = input("What is the primary function of a database? ")
+if answer.lower() == "store and organize data":
     print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!") 
 
-
-answer = input()
-if answer.upper == "A full stack developer":
+answer = input("What is a commonly used version control system for software development? ")
+if answer.lower() == "git":
     print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!") 
 
-answer = input()
-if answer.upper == "A full stack developer":
+answer = input("What is the purpose of a loop in programming? ")
+if answer.lower() == "repeat a sequence of instructions":
     print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!")  
 
-answer = input()
-if answer.upper == "A full stack developer":
+answer = input("What is an API? ")
+if answer.lower() == "application programming interface":
     print('Correct!')
-    score+=1
-
-
-else: 
-    print("Wrong!")          
-
-answer = input()
-if answer.upper == "A full stack developer":
-    print('Correct!')
-    score+=1
-
+    score += 1
 else: 
     print("Wrong!")        
 
-print("You got" + str((score / len(answer) * 100) + "%.") + "questions correct!")
+answer = input("What is the purpose of a function in programming? ")
+if answer.lower() == "perform a specific task":
+    print('Correct!')
+    score += 1
+else: 
+    print("Wrong!")        
+
+print("You got " + str((score / 8) * 100) + "% of questions correct!") 
